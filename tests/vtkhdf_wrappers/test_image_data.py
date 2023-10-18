33caee0aa82512292a840646afd40b534265e188
jmag <jmag722@gmail.com>
2023-10-17 23:30:10 -0500

    JMM[#2]: read write slices added and tested

--- tests/vtkhdf_wrappers/test_image_data.py
+++ tests/vtkhdf_wrappers/test_image_data.py
@@ -1,8 +1,85 @@
 import pytest
+import numpy as np
+import pyvista
+import h5py
+import vtkhdf_wrappers.array_helpers as ah
+import vtkhdf_wrappers.image_data as ida
 
-def test_read_slice(tmp_path):
-    # add once write is broken up
-    ...
+@pytest.fixture
+def radial_box():
+    def _method():
+        dimensions = np.array([151, 91, 113])
+        spacing = np.array([.01, .011, .03])
+        origin = ah.compute_origin(dimensions, spacing)
+        box = pyvista.ImageData(
+            dimensions=dimensions,
+            spacing=spacing,
+            origin=origin
+        )
+        X,Y,_ = ah.mesh(*ah.compute_axis_arrays(box.dimensions, box.spacing,
+                                                box.origin))
+        data = np.sqrt(X*X+Y*Y)
+        ida.set_var(box, data, "data")
+        return box
+    return _method
 
-def test_write_vtkhdf():
-    ...
+def test_read_slice(tmp_path, radial_box):
+    box = radial_box()
+    ida.write_vtkhdf(tmp_path/"mybox-vti.hdf", box)
+    h5_file = h5py.File(tmp_path/"mybox-vti.hdf", "r")
+    for i in range(box.dimensions[2]):
+        slice = ida.read_slice(h5_file, "data", i)
+        assert slice.flags.f_contiguous
+        assert slice.shape == box.dimensions[:-1]
+        np.testing.assert_allclose(slice, ida.get_var(box, "data")[:,:,i])
+    h5_file.close()
+
+def test_init_vtkhdf(tmp_path):
+    h5_file = h5py.File(tmp_path/"foo.hdf", "w")
+    extent = (0,10,0,11,0,13)
+    origin = (.1,.1,0)
+    spacing = (1,2,4)
+    direction = (0,0,1,1,0,0,0,1,0)
+    ida.init_vtkhdf(h5_file, extent, origin=origin,
+                    spacing=spacing, direction=direction)
+    assert bool(h5_file["VTKHDF"])
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Version"],
+                            np.array([1,0]))
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Type"],
+                            b"ImageData")
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Type"],
+                            b"ImageData")
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["WholeExtent"],
+                            extent)
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Origin"],
+                            origin)
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Spacing"],
+                            spacing)
+    np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Direction"],
+                            direction)
+    assert bool(h5_file["VTKHDF"]["PointData"])
+    h5_file.close()
+
+def test_create_dataset(tmp_path):
+    h5_file = h5py.File(tmp_path/"foo.hdf", "w")
+    dim = (11,23,15)
+    extent=(0,10,0,22,0,14)
+    ida.init_vtkhdf(h5_file, extent)
+    ida.create_dataset(h5_file, dim, "myvar")
+    assert h5_file["VTKHDF"]["PointData"].attrs["Scalars"] == b"myvar"
+    h5_file.close()
+
+def test_write_slice(tmp_path, radial_box):
+    box = radial_box()
+    arr = ida.get_var(box, "data")
+    with h5py.File(tmp_path/"foo.hdf", "w") as h5_file:
+        ida.init_vtkhdf(h5_file, box.extent)
+        ida.create_dataset(h5_file, box.dimensions, "newvar")
+        for i in range(box.dimensions[2]):
+            ida.write_slice(h5_file, arr[:,:,i], "newvar", i)
+
+    with h5py.File(tmp_path/"foo.hdf", "r") as h5_file:
+        for i in range(box.dimensions[2]):
+            slice = ida.read_slice(h5_file, "newvar", i)
+            assert slice.shape == box.dimensions[:-1]
+            np.testing.assert_allclose(slice, ida.get_var(box, "data")[:,:,i])
--- vtkhdf_wrappers/image_data.py
+++ vtkhdf_wrappers/image_data.py
@@ -9,9 +9,39 @@ def read_vtkhdf(filename:str):
     reader.Update()    
     return pyvista.wrap(reader.GetOutput())
 
-def read_slice(hdf5_file:h5py.File, var:str, zindex:int):    
+def read_slice(hdf5_file:h5py.File, var:str, zindex:int) -> np.ndarray:    
     dat = hdf5_file["VTKHDF"]["PointData"][var][zindex, :, :]
     return np.asfortranarray(np.transpose(dat))
+
+def init_vtkhdf(file:h5py.File, extent, origin=(0,0,0), spacing=(1,1,1),
+                direction=(1, 0, 0, 0, 1, 0, 0, 0, 1)):
+    vtkhdf_group = file.create_group("VTKHDF")
+    vtkhdf_group.attrs.create("Version", [1, 0])
+    vtkhdf_group.attrs.create("Type", np.string_("ImageData"))
+    vtkhdf_group.attrs.create("WholeExtent", extent)
+    vtkhdf_group.attrs.create("Origin", origin)
+    vtkhdf_group.attrs.create("Spacing", spacing)
+    vtkhdf_group.attrs.create("Direction", direction)
+    vtkhdf_group.create_group("PointData")
+
+def create_dataset(file:h5py.File, shape, name:str):
+    field_data_group = file["VTKHDF"]["PointData"]
+    new_shape = shape[::-1]
+    chunk_shape = np.copy(new_shape)
+    chunk_shape[0] = 1 # z will be at index 0
+    field_data_group.attrs.create("Scalars", np.string_(name))   
+    field_data_group.create_dataset(
+        name, shape=new_shape, chunks=tuple(chunk_shape)
+    )
+
+def write_slice(file:h5py.File, array:np.ndarray, name:str,
+                zindex:int):
+    field_data_group = file["VTKHDF"]["PointData"]
+    if not array.flags.f_contiguous:
+        raise TypeError("Input must be column-major (Fortran) array.")
+    # transpose b/c paraview will be transposing it to read
+    arr_c = np.transpose(array)
+    field_data_group[name][zindex,:,:] = arr_c[np.newaxis,:,:]
 
 def write_vtkhdf(filename:str, imagedata,
                  direction=(1, 0, 0, 0, 1, 0, 0, 0, 1),
@@ -19,27 +49,12 @@ def write_vtkhdf(filename:str, imagedata,
     if type(imagedata) is vtk.vtkImageData:
         imagedata:pyvista.ImageData = pyvista.wrap(imagedata)
     with h5py.File(filename, "w", **kwargs) as f:
-        vtkhdf_group = f.create_group("VTKHDF")
-        vtkhdf_group.attrs.create("Version", [1, 0])
-        vtkhdf_group.attrs.create("Type", np.string_("ImageData"))
-        vtkhdf_group.attrs.create("WholeExtent", imagedata.extent)
-        vtkhdf_group.attrs.create("Origin", imagedata.origin)
-        vtkhdf_group.attrs.create("Spacing", imagedata.spacing)
-        vtkhdf_group.attrs.create("Direction", direction)
-
-        field_data_group = vtkhdf_group.create_group("PointData")
+        init_vtkhdf(f, imagedata.extent, origin=imagedata.origin,
+                    spacing=imagedata.spacing, direction=direction)
         for var in imagedata.array_names:
-            # transpose b/c paraview will be transposing it to read
-            array = np.transpose(get_var(imagedata, var))
-            new_shape = imagedata.dimensions[::-1]
-            chunk_shape = np.copy(new_shape)
-            chunk_shape[0] = 1 # z will be at index 0
-            field_data_group.attrs.create("Scalars", np.string_(var))   
-            dset = field_data_group.create_dataset(
-                var, shape=new_shape, chunks=tuple(chunk_shape)
-            )
+            create_dataset(f, imagedata.dimensions, var)
             for i in range(imagedata.dimensions[2]):
-                dset[i,:,:] = array[i,:,:]
+                write_slice(f, get_var(imagedata, var)[:,:,i], var, i)
 
 def set_var(image_data:pyvista.ImageData, array:np.ndarray, name:str):
     image_data[name] = array.flatten(order="F")
