1b5d408864fe82e328650c71a455de8d4706a8f3
jmag <jmag722@gmail.com>
2023-10-18 07:32:45 -0500

    JMM[#1]: array helpers tests complete

--- tests/vtkhdf_wrappers/test_array_helpers.py
+++ tests/vtkhdf_wrappers/test_array_helpers.py
@@ -1,8 +1,33 @@
+import numpy as np
+import vtkhdf_wrappers.array_helpers as ah
+
 def test_compute_origin():
-    ...
+    dim = (15,16,17)
+    spacing = (1,1,3)
+    actual = ah.compute_origin(dim, spacing, True)
+    expected = np.array([-7,-7.5,0])
+    np.testing.assert_equal(actual, expected)
+    actual = ah.compute_origin(dim, spacing, False)
+    expected = np.array([-7,-7.5,-24])
+    np.testing.assert_equal(actual, expected)
+    actual = ah.compute_origin(5, 2)
+    expected = np.array([-4])
+    np.testing.assert_equal(actual, expected)
 
 def test_compute_axis_array():
-    ...
+    np.testing.assert_equal(ah.compute_axis_array(5,1,0),
+                            np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_allclose(ah.compute_axis_array(4,.1,-.5),
+                               np.array([-.5, -.4, -.3, -.2]))
 
 def test_compute_axis_arrays():
-    ...
+    dim = (5,4,3)
+    s = (1,2,4)
+    o = (0, 1, .3)
+    x,y,z = ah.compute_axis_arrays(dim, s, o)
+    np.testing.assert_allclose(x,
+                               np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_allclose(y,
+                               np.array([1, 3, 5, 7]))
+    np.testing.assert_allclose(z,
+                               np.array([.3, 4.3, 8.3]))
--- vtkhdf_wrappers/array_helpers.py
+++ vtkhdf_wrappers/array_helpers.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-def compute_origin(dimensions:np.ndarray, spacing:np.ndarray,
-                   zero_z:bool=True):
-    origin = -0.5*(dimensions-1)*spacing
-    if zero_z:
+def compute_origin(dimensions, spacing,
+                   zero_z:bool=True) -> np.ndarray:
+    origin = -0.5*(np.array(dimensions)-1)*np.array(spacing)
+    if zero_z and origin.size == 3:
         origin[2] = 0.0
     return origin
 
-def compute_axis_array(d:float, s:float, o:float):
-    return np.linspace(o, o + (d-1)*s, d, endpoint=True)
+def compute_axis_array(dim:float, step:float, origin:float) -> np.ndarray:
+    return np.linspace(origin, origin + (dim-1)*step, dim,
+                       endpoint=True)
 
 def compute_axis_arrays(dimensions, spacing, origin):
     x = compute_axis_array(dimensions[0], spacing[0], origin[0])
