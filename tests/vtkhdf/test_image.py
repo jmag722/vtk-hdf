72a349f25ef64eae451ab5e80c3ea3967b716892
jmag <jmag722@gmail.com>
2023-10-20 07:15:04 -0500

    JMM[#11]: simplified function names, merged utils into imagedata module, as they're all to facilitate those calls

--- tests/vtkhdf/test_image.py
+++ tests/vtkhdf/test_image.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 import pyvista
 import h5py
-import vtkhdf.utils as ut
 import vtkhdf.image as v5i
 
 @pytest.fixture
@@ -10,16 +9,16 @@ def radial_box():
     def _method():
         dimensions = np.array([151, 91, 113])
         spacing = np.array([.01, .011, .03])
-        origin = ut.compute_origin(dimensions, spacing)
+        origin = v5i.center_origin(dimensions, spacing)
         box = pyvista.ImageData(
             dimensions=dimensions,
             spacing=spacing,
             origin=origin
         )
-        X,Y,_ = ut.mesh(*ut.compute_axis_arrays(box.dimensions, box.spacing,
+        X,Y,_ = v5i.mesh_axes(*v5i.get_axes(box.dimensions, box.spacing,
                                                 box.origin))
         data = np.sqrt(X*X+Y*Y)
-        v5i.set_imagedata(box, data, "data")
+        v5i.set_array(box, data, "data")
         return box
     return _method
 
@@ -31,23 +30,23 @@ def test_read_slice(tmp_path, radial_box):
         slice = v5i.read_slice(h5_file, "data", i)
         assert slice.flags.f_contiguous
         assert slice.shape == box.dimensions[:-1]
-        np.testing.assert_allclose(slice, v5i.get_imagedata(box, "data")[:,:,i])
+        np.testing.assert_allclose(slice, v5i.get_array(box, "data")[:,:,i])
     h5_file.close()
 
 def test_read_vtkhdf(tmp_path, radial_box):
     box = radial_box()
     v5i.write_vtkhdf(tmp_path/"mybox-vti.hdf", box)
     readin = v5i.read_vtkhdf(tmp_path/"mybox-vti.hdf")
-    np.testing.assert_allclose(v5i.get_imagedata(box, "data"),
-                               v5i.get_imagedata(readin, "data"))
+    np.testing.assert_allclose(v5i.get_array(box, "data"),
+                               v5i.get_array(readin, "data"))
 
-def test_init_vtkhdf(tmp_path):
+def test_initialize(tmp_path):
     h5_file = h5py.File(tmp_path/"foo.hdf", "w")
     extent = (0,10,0,11,0,13)
     origin = (.1,.1,0)
     spacing = (1,2,4)
     direction = (0,0,1,1,0,0,0,1,0)
-    v5i.init_vtkhdf(h5_file, extent, origin=origin,
+    v5i.initialize(h5_file, extent, origin=origin,
                     spacing=spacing, direction=direction)
     assert bool(h5_file["VTKHDF"])
     np.testing.assert_equal(h5_file.get("VTKHDF").attrs["Version"],
@@ -70,7 +69,7 @@ def test_init_vtkhdf(tmp_path):
 def test_create_dataset(tmp_path):
     h5_file = h5py.File(tmp_path/"foo.hdf", "w")
     dim = (11,23,15)
-    v5i.init_vtkhdf(h5_file, v5i.dimensions2extent(dim))
+    v5i.initialize(h5_file, v5i.dimensions2extent(dim))
     v5i.create_dataset(h5_file, "myvar")
     assert h5_file["VTKHDF"]["PointData"].attrs["Scalars"] == b"myvar"
     assert h5_file["VTKHDF"]["PointData"]["myvar"].shape == (15,23,11)
@@ -80,7 +79,7 @@ def test_create_dataset(tmp_path):
 def test_create_dataset_c(tmp_path):
     h5_file = h5py.File(tmp_path/"foo_c.hdf", "w")
     dim_c = (11,23,15)
-    v5i.init_vtkhdf(h5_file, v5i.dimensions2extent(dim_c[::-1]))
+    v5i.initialize(h5_file, v5i.dimensions2extent(dim_c[::-1]))
     v5i.create_dataset(h5_file, "myvar")
     assert h5_file["VTKHDF"]["PointData"].attrs["Scalars"] == b"myvar"
     assert h5_file["VTKHDF"]["PointData"]["myvar"].shape == (11,23,15)
@@ -89,9 +88,9 @@ def test_create_dataset_c(tmp_path):
 
 def test_write_slice(tmp_path, radial_box):
     box = radial_box()
-    arr = v5i.get_imagedata(box, "data")
+    arr = v5i.get_array(box, "data")
     with h5py.File(tmp_path/"foo.hdf", "w") as h5_file:
-        v5i.init_vtkhdf(h5_file, box.extent)
+        v5i.initialize(h5_file, box.extent)
         v5i.create_dataset(h5_file, "newvar")
         for i in range(box.dimensions[2]):
             v5i.write_slice(h5_file, arr[:,:,i], "newvar", i)
@@ -100,14 +99,14 @@ def test_write_slice(tmp_path, radial_box):
         for i in range(box.dimensions[2]):
             slice = v5i.read_slice(h5_file, "newvar", i)
             assert slice.shape == box.dimensions[:-1]
-            np.testing.assert_allclose(slice, v5i.get_imagedata(box, "data")[:,:,i])
+            np.testing.assert_allclose(slice, v5i.get_array(box, "data")[:,:,i])
 
 def test_write_slice_c(tmp_path):
     shape_c = (1,10,4)
     arr = np.random.rand(*shape_c)
     file = "foo_c.hdf"
     with h5py.File(tmp_path/file, "w") as h5_file:
-        v5i.init_vtkhdf(h5_file, v5i.dimensions2extent(shape_c[::-1]))
+        v5i.initialize(h5_file, v5i.dimensions2extent(shape_c[::-1]))
         v5i.create_dataset(h5_file, "newvar")
         v5i.write_slice(h5_file, arr[0,:,:], "newvar", 0)
 
@@ -121,5 +120,36 @@ def test_dimensions2extent():
     assert v5i.dimensions2extent((5,3,1,2)) == (0,4,0,2,0,0,0,1)
 
 def test_extent2dimensions():
-    assert v5i.extent2dimension((0,5,0,3,0,4)) == (6,4,5)
-    assert v5i.extent2dimension((0,15,0,1,0,0,0,3)) == (16,2,1,4)
+    assert v5i.extent2dimensions((0,5,0,3,0,4)) == (6,4,5)
+    assert v5i.extent2dimensions((0,15,0,1,0,0,0,3)) == (16,2,1,4)
+
+def test_center_origin():
+    dim = (15,16,17)
+    spacing = (1,1,3)
+    actual = v5i.center_origin(dim, spacing, True)
+    expected = np.array([-7,-7.5,0])
+    np.testing.assert_equal(actual, expected)
+    actual = v5i.center_origin(dim, spacing, False)
+    expected = np.array([-7,-7.5,-24])
+    np.testing.assert_equal(actual, expected)
+    actual = v5i.center_origin(5, 2)
+    expected = np.array([-4])
+    np.testing.assert_equal(actual, expected)
+
+def test_get_axis():
+    np.testing.assert_equal(v5i.get_axis(5,1,0),
+                            np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_allclose(v5i.get_axis(4,.1,-.5),
+                               np.array([-.5, -.4, -.3, -.2]))
+
+def test_get_axes():
+    dim = (5,4,3)
+    s = (1,2,4)
+    o = (0, 1, .3)
+    x,y,z = v5i.get_axes(dim, s, o)
+    np.testing.assert_allclose(x,
+                               np.array([0, 1, 2, 3, 4]))
+    np.testing.assert_allclose(y,
+                               np.array([1, 3, 5, 7]))
+    np.testing.assert_allclose(z,
+                               np.array([.3, 4.3, 8.3]))
--- vtkhdf/image.py
+++ vtkhdf/image.py
@@ -19,7 +19,7 @@ def read_slice(hdf5_file:h5py.File, var:str, zindex:int) -> np.ndarray:
     dat = hdf5_file["VTKHDF"]["PointData"][var][zindex, :, :]
     return c2f_reshape(dat)
 
-def init_vtkhdf(file:h5py.File, extent, origin=(0,0,0), spacing=(1,1,1),
+def initialize(file:h5py.File, extent, origin=(0,0,0), spacing=(1,1,1),
                 direction=(1, 0, 0, 0, 1, 0, 0, 0, 1)):
     vtkhdf_group = file.create_group("VTKHDF")
     vtkhdf_group.attrs.create("Version", [1, 0])
@@ -33,7 +33,7 @@ def init_vtkhdf(file:h5py.File, extent, origin=(0,0,0), spacing=(1,1,1),
 def create_dataset(file:h5py.File, name:str):
     field_data_group = file["VTKHDF"]["PointData"]
     # reverse what is given in WholeExtent so paraview can read
-    shape_c = extent2dimension(file["VTKHDF"].attrs["WholeExtent"])[::-1]
+    shape_c = extent2dimensions(file["VTKHDF"].attrs["WholeExtent"])[::-1]
     chunk_shape = (1, *shape_c[1:]) # single z-slices, z will be at index 0
     field_data_group.attrs.create("Scalars", np.string_(name))   
     field_data_group.create_dataset(
@@ -52,21 +52,41 @@ def write_vtkhdf(filename:str, imagedata,
     if type(imagedata) is vtk.vtkImageData:
         imagedata:pyvista.ImageData = pyvista.wrap(imagedata)
     with h5py.File(filename, "w", **kwargs) as f:
-        init_vtkhdf(f, imagedata.extent, origin=imagedata.origin,
+        initialize(f, imagedata.extent, origin=imagedata.origin,
                     spacing=imagedata.spacing, direction=direction)
         for var in imagedata.array_names:
             create_dataset(f, var)
             for i in range(imagedata.dimensions[2]):
-                write_slice(f, get_imagedata(imagedata, var)[:,:,i], var, i)
+                write_slice(f, get_array(imagedata, var)[:,:,i], var, i)
 
-def set_imagedata(image_data:pyvista.ImageData, array:np.ndarray, name:str):
+def set_array(image_data:pyvista.ImageData, array:np.ndarray, name:str):
     image_data[name] = array.flatten(order="F")
 
-def get_imagedata(image_data:pyvista.ImageData, name:str):
+def get_array(image_data:pyvista.ImageData, name:str):
     return image_data[name].reshape(image_data.dimensions, order="F")
 
 def dimensions2extent(dimensions):
     return tuple(x for v in dimensions for x in (0,v-1))
 
-def extent2dimension(extent):
+def extent2dimensions(extent):
     return tuple(x+1 for x in extent[1::2])
+
+def center_origin(dimensions, spacing,
+                   zero_z:bool=True) -> np.ndarray:
+    origin = -0.5*(np.array(dimensions)-1)*np.array(spacing)
+    if zero_z and origin.size == 3:
+        origin[2] = 0.0
+    return origin
+
+def get_axis(dimension:float, spacing:float, origin:float) -> np.ndarray:
+    return np.linspace(origin, origin + (dimension-1)*spacing, dimension,
+                       endpoint=True)
+
+def get_axes(dimensions, spacing, origin):
+    x = get_axis(dimensions[0], spacing[0], origin[0])
+    y = get_axis(dimensions[1], spacing[1], origin[1])
+    z = get_axis(dimensions[2], spacing[2], origin[2])
+    return x,y,z
+
+def mesh_axes(x,y,z):
+    return np.meshgrid(x, y, z, indexing="ij")
